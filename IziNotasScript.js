92fa550e7149605c93e221a45dd49c729ad6ee69
Alberto Camargo <amcamargo95@gmail.com>
2015-11-21 20:37:35 -0500

    Set interval defined and route stablish

--- IziNotasScript.js
+++ IziNotasScript.js
@@ -1,4 +1,13 @@
 var fs = require('fs');
-var filePath = "cookies.json" ;
-fs.unlinkSync(filePath);
-fs.createWriteStream(filePath);
+// 3600000 = 1 hour
+setInterval(deleteFile, 3600000);
+
+function deleteFile() {
+
+  var filePath = "../izinotas/auroraApp/cookies.json" ;
+  fs.unlinkSync(filePath);
+  console.log("cleaning < cookies.json >...");
+  fs.createWriteStream(filePath);
+}
+
+
